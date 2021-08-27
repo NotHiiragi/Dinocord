3a19fbec4f68d728f82fbed52cb1da7408640f78
Vilmer Johansson <47473249+WaffleTheCool@users.noreply.github.com>
2021-08-27 17:44:04 +0200

    Updating

--- src/client/Client.ts
+++
@@ -1,7 +0,0 @@
-import EventEmitter from 'https://deno.land/std@0.51.0/node/events.ts';
-import WebSocketManager from '../ws/WebSocketManager.ts';
-
-export default class Client extends EventEmitter {
-
-    //private socket: WebSocketManager = new WebSocketManager(this);
-}
--- src/interfaces/Payload.ts
+++
@@ -1,6 +0,0 @@
-export interface Payload {
-    op: number;
-    t: string;
-    s: number;
-    d: any;
-}
