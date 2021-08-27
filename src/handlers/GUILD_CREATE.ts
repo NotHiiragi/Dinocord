f2f15891d1eac3276a268229e27d4469083f734d
Vilmer Johansson <47473249+WaffleTheCool@users.noreply.github.com>
2021-08-27 17:44:33 +0200

    Update complete

---
+++ src/handlers/GUILD_CREATE.ts
@@ -0,0 +1,7 @@
+import Client from "../client/Client.ts";
+import { Payload } from "../interfaces/Payload.ts"
+
+export default function(client: Client, payload: Payload) {
+
+    console.log(payload);
+}
---
+++ src/handlers/READY.ts
@@ -0,0 +1,18 @@
+import Client from "../client/Client.ts";
+import { Payload } from "../interfaces/Payload.ts";
+import ClientUser from "../client/ClientUser.ts";
+
+export default function(client: Client, payload: Payload) {
+    const { user } = payload.d;
+    client.user = new ClientUser(
+        user.username,
+        user.discriminator,
+        user.verified,
+        user.id,
+        user.flags,
+        user.email,
+        user.bot,
+        user.avatar
+    );
+    client.emit('ready');
+}
