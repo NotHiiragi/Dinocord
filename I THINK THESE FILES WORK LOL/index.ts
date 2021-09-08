964c7ad58963cde0e2e0b2013a81e74411284fad
Vilmer Johansson <47473249+WaffleTheCool@users.noreply.github.com>
2021-09-08 20:28:23 +0200

    Update index.ts

--- I THINK THESE FILES WORK LOL/index.ts
+++ I THINK THESE FILES WORK LOL/index.ts
@@ -24,7 +24,7 @@ async function createMessage(content: string, channelId: string) {
         "tts": false,
     };
 
-    const headers = { 'Content-Type': 'application/json', 'Authorization': `Bot ODc5NzY1NTYyMjk5OTI4NjY2.YSUfhw.TgOt1ox01PZnpCduEmjWe9-UECE`};
+    const headers = { 'Content-Type': 'application/json', 'Authorization': `Bot TOKEN`};
 
     const response = await fetch(`${Constants.API}/channels/${channelId}/messages`, {
         method: 'POST',
