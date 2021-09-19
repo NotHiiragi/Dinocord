a338ed2b691aff54dee4959f6bc8e491f8ad3599
Vilmer Johansson <47473249+WaffleTheCool@users.noreply.github.com>
2021-09-19 11:43:05 +0200

    edit console.log from debugging

--- src/handlers/GUILD_CREATE.ts
+++ src/handlers/GUILD_CREATE.ts
@@ -2,5 +2,5 @@ import { Client } from "../client/Client.ts";
 import { Payload } from "../interfaces/Payload.ts"
 
 export default function(client: Client, payload: Payload) {
- console.log("GUILD_CREATE was succesfull");
+// console.log("GUILD_CREATE was succesfull");
 }
