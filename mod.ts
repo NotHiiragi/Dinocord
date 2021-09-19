3a59aec76f2dda0fa234379c0f68a024979cf440
Vilmer Johansson <47473249+WaffleTheCool@users.noreply.github.com>
2021-09-19 11:43:45 +0200

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,4 +1,2 @@
 export { Client } from './src/client/Client.ts';
-export { Constants } from './src/constants/Constants.ts';
-export { headers } from './src/constants/Payloads.ts';
 export { createMessage } from './src/models/Message.ts';
